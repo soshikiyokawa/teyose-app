dda7f5e5575371dc0a7ffe9c393889658c7d2381
soshikiyokawa <so.kiyokawa@kiyokawanoie.com>
2026-06-28 16:42:25 +0900

    サービスワーカーの更新が即時反映されるように修正
    skipWaiting/clients.claimが無く、タブを完全に閉じない限り古いコード
    が動き続けていた。これにより過去の修正が反映されていなかった可能性。

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'teyose-v18';
+const CACHE_NAME = 'teyose-v19';
 const ASSETS = [
   './',
   './index.html',
@@ -38,12 +38,13 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', e=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));
+  e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
 });
 
 self.addEventListener('activate', e=>{
   e.waitUntil(
     caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
   );
 });
 
